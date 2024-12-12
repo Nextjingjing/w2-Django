653711e49c1d638f1e1cf597864af4cc1602f580
SNeXT <snext2004@gmail.com>
2024-12-12 15:24:43 +0700

    dir and time

--- note/urls.py
+++ note/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import NoteListView, NoteDetailView
+from .views import NoteListView, NoteDetailView, current_time, list_directory
 
 app_name = "note"
 urlpatterns = [
     path('', NoteListView.as_view(), name='index'),  # ใช้ ListView
     path('<int:pk>/', NoteDetailView.as_view(), name='detail'),  # ใช้ DetailView
+    path('time/', current_time, name='time'),
+    path('dir/', list_directory , name='dir')
 ]
--- note/views.py
+++ note/views.py
@@ -1,13 +1,44 @@
 from django.views.generic import ListView, DetailView
-
+from django.utils import timezone
 from .models import Note
+from django.http import HttpResponse
+from django.conf import settings
+import os
+from django.shortcuts import render
+import os
+
 
 class NoteListView(ListView):
     model = Note
-    template_name = 'note/index.html'  # ชื่อไฟล์ Template
-    context_object_name = 'notes'     # ชื่อ Context ที่ส่งไป Template (default: 'object_list')
+    template_name = 'note/index.html'  
+    context_object_name = 'notes'     
 
 class NoteDetailView(DetailView):
     model = Note
     template_name = 'note/detail.html' 
-    context_object_name = 'note'       
+    context_object_name = 'note'
+
+def current_time(request):
+    now = timezone.now()
+    return HttpResponse(f"Current time: {now.strftime('%H:%M:%S')}")
+
+def list_directory(request):
+    directory_path = request.GET.get('directory_path', '.')
+    try:
+        files_and_folders = os.listdir(directory_path)
+
+        items = [
+            {
+                "name": item,
+                "is_dir": os.path.isdir(os.path.join(directory_path, item)),
+                "path": os.path.join(directory_path, item),
+            }
+            for item in files_and_folders
+        ]
+        return render(request, 'note/dir.html', {
+            "directory": directory_path,
+            "contents": items,
+        })
+    except FileNotFoundError:
+        return HttpResponse('error')
+
